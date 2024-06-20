882907d08fdaf41aaadc878664419aaacdc9277e
brahmdi <hmdibrahim09@gmail.com>
2024-06-20 14:42:39 +0100

    web service avec soap

---
+++ ws-soap/src/main/java/ServerJWS.java
@@ -0,0 +1,11 @@
+import jakarta.xml.ws.Endpoint;
+import ws.BanqueService;
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        String url="http://0.0.0.0:9090/";
+        Endpoint.publish(url,new BanqueService());
+        System.out.println("Le WSDL est un Document XML qui permet de faire la description d'une interface d'un web Service ");
+        System.out.println("Web Service déployé sur :"+url);
+    }
+}
